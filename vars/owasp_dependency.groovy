96bf7e2b5e690da3f5b9151c74886a0a6839e434
iamshubhamsain <43787362+shubhamsain@users.noreply.github.com>
2025-09-11 00:11:39 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,5 +1,7 @@
 def call() {
     echo 'Running OWASP Dependency Check via Jenkins Plugin...'
     dependencyCheck additionalArguments: '--scan ./ --format XML --disableAssembly', odcInstallation: 'OWASP'
+    dependencyCheck additionalArguments: '--scan ./ --format XML --disableYarnAudit', odcInstallation: 'OWASP'
+
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
