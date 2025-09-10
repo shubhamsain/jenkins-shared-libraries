799363878854bad2c1aad802b508b88b8f2fec9c
iamshubhamsain <43787362+shubhamsain@users.noreply.github.com>
2025-09-11 00:01:39 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,10 +1,16 @@
-stage('OWASP: Dependency check') {
-    steps {
-        script {
-            dependencyCheck additionalArguments: '', 
-                            outdir: 'dependency-check-report', 
-                            scanpath: '.', 
-                            datadir: ''
-        }
-    }
+def call() {
+    echo "Running OWASP Dependency Check..."
+
+    // The actual OWASP check command
+    sh '''#!/bin/bash
+    dependency-check.sh \
+        --project "Wanderlust" \
+        --scan ./ \
+        --format XML \
+        --out . \
+        --disableAssembly
+    '''
+    
+    // Publish the report
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
